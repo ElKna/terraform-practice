3217168c5fccdd75c5bf81352f5985d08878e880
ElKna <yyt012@gmail.com>
2023-08-02 03:18:25 +0900

    Add: Add data policy document and apply policy

--- main.tf
+++ main.tf
@@ -37,16 +37,30 @@ resource "aws_iam_policy" "policy" {
     name        = "test_policy"
     path        = "/"
     description = "My test policy"
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = [
-                    "ec2:Describe*"
-                ]
-                Effect      = "Allow"
-                Resource    = "*"
-            },
+    policy      = data.aws_iam_policy_document.this.json
+    # policy = jsonencode({
+    #     Version = "2012-10-17"
+    #     Statement = [
+    #         {
+    #             Action = [
+    #                 "ec2:Describe*"
+    #             ]
+    #             Effect      = "Allow"
+    #             Resource    = "*"
+    #         },
+    #     ]
+    # })
+}
+
+data "aws_iam_policy_document" "this" {
+    statement {
+        sid     = "1"
+        effect  = "Allow"
+        actions = [
+            "sts:AssumeRole",
         ]
-    })
+        resources = [
+            "arn:aws:s3:::*",
+        ]
+    }
 }

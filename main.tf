91c207c8179a5749fc08b36694c5c5721a2956af
ElKna <yyt012@gmail.com>
2023-08-02 04:20:58 +0900

    Add: Create IAM user by for_each condition

--- main.tf
+++ main.tf
@@ -2,7 +2,40 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
+# resource "aws_iam_user" "this" {
+#   count = 4
+#   name = "trial-${count.index}"
+# }
+
+# output "users" {
+#     # value = aws_iam_user.this[0].name
+#     value = aws_iam_user.this[*].name
+# }
+
+variable "users" {
+    # type = object({
+    #   rex       = string
+    #   vincent   = string
+    #   june      = string
+    # })
+    type = map(string)
+    default = {
+        "rex"       = "/path1/"
+        "vincent"   = "/path2/"
+        "june"      = "/path3/"
+    }
+}
+
 resource "aws_iam_user" "this" {
-  count = 4
-  name = "trial-${count.index}"
+    for_each    = var.users
+    name        = each.key
+    path        = each.value
+}
+
+output "test" {
+    # value = aws_iam_user.this
+    # 아래 명령어는 key와 value를 통해 새로운 key, value를 생성하는 명령어임
+    value = { for k, v in aws_iam_user.this : k => v.arn}
+    # 아래 명령어는 위와 비슷하지만 List로 만들게 됨
+    # value = [for _, v in aws_iam_user.this : v.arn]
 }

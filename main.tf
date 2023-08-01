efe12e8f27d9da8a0ba5a6a567534edc6ff74b7f
ElKna <yyt012@gmail.com>
2023-08-02 02:54:21 +0900

    Fix: Add IAM Role

--- main.tf
+++ main.tf
@@ -2,16 +2,19 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
-data "aws_caller_identity" "current" {}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "caller_arn" {
-  value = data.aws_caller_identity.current.arn
-}
-
-output "caller_user" {
-  value = data.aws_caller_identity.current.user_id
+resource "aws_iam_role" "this" {
+    name = "test_role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Sid = ""
+                Principal = {
+                    Service = "ec2.amazonaws.com"
+                }
+            }, 
+        ]
+    })
 }
